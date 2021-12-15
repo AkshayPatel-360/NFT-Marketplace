fe08d96138bb4ec049d6045edf2e971413e66eae
AkshayPatel-360 <axaymow@gmail.com>
2021-12-15 14:54:22 -0500

    created KryptoBirdz.sol

---
+++ src/contracts/KryptoBirdz.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract KryptoBirdz{
+    string private name;
+    string private symbol;
+
+    constructor() {
+        name = 'KryptoBirdz';
+        symbol = 'KBIRDZ';
+    }
+}
