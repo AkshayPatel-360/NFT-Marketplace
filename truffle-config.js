43941d41e71d31873b81b863ef7d02797503c889
AkshayPatel-360 <axaymow@gmail.com>
2021-12-15 14:55:41 -0500

    Deployed smart contract successfully

--- truffle-config.js
+++ truffle-config.js
@@ -7,10 +7,12 @@ module.exports = {
      port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
-
-    contracts_directory: './src/contracts',
-    contracts_build_directory: './src/abis',  
+ 
   },
+
+  
+  contracts_directory: './src/contracts/',
+  contracts_build_directory: './src/abis', 
 
   compilers: {
     solc: {
